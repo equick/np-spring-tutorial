150d7c6059a00d549cad2f98971ff96d3a1b0da2
equick <edwardquick@hotmail.com>
2016-07-28 18:10:17 +0100

    First Commit

---
+++ src/main/java/org/linuxproblems/spring/tutorial/NpSpringTutorialApplication.java
@@ -0,0 +1,12 @@
+package org.linuxproblems.spring.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpSpringTutorialApplication.class, args);
+	}
+}
