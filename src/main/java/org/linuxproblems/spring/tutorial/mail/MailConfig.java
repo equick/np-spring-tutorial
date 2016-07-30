95dd025479bb134c2c784c51751e3d3e785ce4ac
equick <edwardquick@hotmail.com>
2016-07-30 12:23:39 +0100

    Conditional Annotations

--- src/main/java/org/linuxproblems/spring/tutorial/mail/MailConfig.java
+++ src/main/java/org/linuxproblems/spring/tutorial/mail/MailConfig.java
@@ -1,5 +1,6 @@
 package org.linuxproblems.spring.tutorial.mail;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,13 +9,15 @@ import org.springframework.context.annotation.Profile;
 public class MailConfig {
 	
 	@Bean
-	@Profile("dev")
+	@ConditionalOnProperty(name="spring.mail.host",
+	                       havingValue="foo",
+	                       matchIfMissing=true)
 	public MailSender mockMailSender() {
 		return new MockMailSender();
 	}
 	
 	@Bean
-	@Profile("!dev")
+	@ConditionalOnProperty("spring.mail.host")
 	public MailSender smtpMailSender() {
 		return new SmtpMailSender();
 	}
